1ffe075dcbc1756da9609130cadb25925c40cf6d
nivethi-ai <nivethikagopalakrish@gmail.com>
2024-10-05 11:19:58 +0530

    added Questions

---
+++ index.js
@@ -0,0 +1,56 @@
+var product="milk"
+console.log(product)
+var count=5
+var price=100
+total=price*count
+console.log(total)
+
+
+var age=18
+if (age>=18){
+    a="you can drive"}
+    
+
+else{
+   b="you can't drive"}
+   console.log(a)
+
+   
+
+var product="cup"   
+var count=5
+var price=45
+var total=(count*price)*90/100//10% discount
+
+console.log(product)
+console.log(total)
+
+
+
+var hour=6
+if(5<=hour<12){
+   Greeting="morning"
+}
+else{
+    if(12<=hour<17) {
+       Greeting2="Afternoon"
+    }
+    
+}
+if(17<=hour<21){
+     Greeting3="Evening"
+    
+}
+else{
+    if(21<=hour<5){
+        Greeting4="Night"
+    }
+}
+console.log(greeting)
+
+
+
+
+
+
+
